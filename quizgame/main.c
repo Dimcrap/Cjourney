d7447fb8198ba8efea0b097a510ed141afa7896c
mamadreza <mamad.rahmani235@gmail.com>
2026-08-18 12:30:06 -0400

    comming back to quiz game -test some parsing methods in background with cJSON-getting some quiz example from my new api

--- quizgame/main.c
+++ quizgame/main.c
@@ -1,4 +1,7 @@
 #include <curl/curl.h>
+#include <curl/easy.h>
+#include <curl/typecheck-gcc.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -8,7 +11,7 @@ size_t write_callback(void *ptr,size_t size,size_t nmemb,
 
 	size_t total_size = size * nmemb;
 	//append ptr to buffer
-	retur  total_size;
+	return  total_size;
 }
 
 
@@ -18,3 +21,30 @@ void fetch_url(const char *url){
 	}
 }
 
+
+
+int main(){
+		CURL * curl;
+		CURLcode result=curl_global_init(CURL_GLOBAL_ALL);
+		if(result!= CURLE_OK)
+			return (int) result;
+
+		curl = curl_easy_init();
+		if(curl){
+			curl_easy_setopt(
+			curl , CURLOPT_URL, "wttr.in/China?format=3");
+			
+			curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION , 1L);
+
+
+			result = curl_easy_perform(curl);
+			if(result != CURLE_OK){
+				fprintf(stderr, "curl_easy_platform() failed: %s\n",
+					 curl_easy_strerror(result));
+				}
+
+			curl_easy_cleanup(curl);
+		};
+
+		curl_global_cleanup();
+}
